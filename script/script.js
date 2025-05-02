fb8ffb5856dd9dfb60c9fda96c03fdc38620adf7
hiboubou <meuana30@ecolecatholique.ca>
2025-05-02 15:23:09 -0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,2 +1,41 @@
-function une() {
-}
+function calculatrice() {
+        //Trouve les données dans le formulaire
+        var operationChoisi = document.getElementsByName("choix");
+        var operation = "";
+        for(var i = 0; i < operationChoisi.length; i++) {
+          if(operationChoisi[i].checked){
+            operation = operationChoisi[i].value;
+          }
+        }
+        console.log(operation);
+        var nombreUn = parseFloat(document.getElementById("nombre1").value);
+        var nombreDeux = parseFloat(document.getElementById("nombre2").value);
+        var affiche = document.getElementById("divAffiche");
+        
+        //Calcule basé sur l'opération choisi
+        var calcule = 0;
+        switch(operation) {
+          case "+":
+            calcule = nombreUn + nombreDeux;
+            break;
+          case "-":
+            calcule = nombreUn - nombreDeux;
+            break;
+          case "*":
+            calcule = nombreUn * nombreDeux;
+            break;
+          case "/":
+            calcule = nombreUn / nombreDeux;
+            break;
+          case "%":
+            calcule = nombreUn % nombreDeux;
+            break;
+          case "**":
+            calcule = nombreUn ** nombreDeux;
+            break;
+        }
+        
+        //Affiche le résultat
+        var resultat = "<br><br> Le modulo de tes entrées est de : " +calcule;
+        affiche.innerHTML = resultat;
+        }
